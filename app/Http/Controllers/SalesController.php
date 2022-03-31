c716b1e33cb3512d645b1277abd39e4d6b397c09
Sumon Ahmed <bdsumon4u@gmail.com>
2022-03-31 17:49:22 +0600

    Search SaleNote

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -92,7 +92,8 @@ class SalesController extends BaseController
                             return $query->whereHas('warehouse', function ($q) use ($request) {
                                 $q->where('name', 'LIKE', "%{$request->search}%");
                             });
-                        });
+                        })
+                        ->orWhere('notes', 'LIKE', "%{$request->search}%");
                 })->when($request->filled('from_date') && $request->filled('to_date'), function ($query) use (&$request) {
                     return $query->whereBetween('date', [$request->from_date, $request->to_date]);
                 });

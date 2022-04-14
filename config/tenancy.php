76ccf5256c7ddf67cdf606564a80544b4820175c
Sumon Ahmed <bdsumon4u@gmail.com>
2022-04-14 15:31:02 +0600

    Get Central Domain From .env

--- config/tenancy.php
+++ config/tenancy.php
@@ -16,10 +16,7 @@ return [
      *
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
-    'central_domains' => [
-        '127.0.0.1',
-        'localhost',
-    ],
+    'central_domains' => [parse_url(env('APP_URL', 'localhost'), PHP_URL_HOST)],
 
     /**
      * Tenancy bootstrappers are executed when tenancy is initialized.
